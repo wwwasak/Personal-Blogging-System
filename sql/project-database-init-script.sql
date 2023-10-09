e797cd5f327ed66dac98368feda9dc9f07cc765c
Arcshic <20130421042jyl@gmail.com>
2023-10-10 11:34:26 +1300

    sql merged from branches

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -1,29 +1,59 @@
-/*
- * Upon submission, this file should contain the SQL script to initialize your database.
- * It should contain all DROP TABLE and CREATE TABLE statments, and any INSERT statements
- * required.
- * 
- * It is recommened for your server automatically create & init the database
- * 
- * However this script will serve as documentation / backup for how your database is designed
- */
-
 /* User table created and 4 rows data filled ---- yji413 */
+
 drop table if exists user;
 
 create table user (
     id integer not null primary key,
     account text,
     password text,
-    token text  
+	birthday text,
+	description text,
+    token text
+);
+
+insert into user (id,account,password,birthday,description) values
+    (1,'Sean','123456','1983/02/24','This is the first user'),
+    (2,'Demo','456234','1990/05/27','This is the second user'),
+    (3,'Clarke','abcdefg','1995/04/23','This is the third user'),
+    (4,'Admin','1wde356fg','1993/02/23','This is the fourth user');
+	
+/* create table named category ---- gli300 */
+DROP TABLE if exists category;
   
-insert into user(id,account,password) values
-    (1,'Sean','123456'),
-    (2,'Demo','456234'),
-    (3,'Clarke','abcdefg'),
-    (4,'Admin','1wde356fg')
+create table category (
+
+ id integer NOT NULL PRIMARY KEY,
+
+ name varchar(50) NOT NULL,
+
+ description varchar(1000) NOT NULL,
+ 
+ userid integer not null,
+ 
+ FOREIGN KEY userid REFERENCES user (id)
+);
+insert into category (id, name, description,userid) values (1, 'fiction', 'a type of literature that describes imaginary people and events, not real ones',1);  
   
  
+ /*table article: More info in interface.md; by zipei liu*/
+
+drop table if exists article;
+
+create table article(
+    id integer not null primary key,
+    title text not null,
+    content text not null,
+    postdate integer,
+    userid integer,
+    categoryid integer,
+    foreign key userid references user(id),
+    foreign key categoryid references category(id)
+);
+insert into artical (id, title, content, userid,categoryid) values
+    (1, 'Love', '<strong>I love you!</strong>' , 3,1)
+    (2, 'Boy', '<em>I am a boy!</em>' , 2,1)
+    (3, 'Game', 'I love play game',1,1 ) 
+
  /* User table created and 4 rows data filled ---- Ricky */
 DROP TABLE if exists userlike;
 
@@ -35,43 +65,15 @@ CREATE TABLE userlike (
     FOREIGN KEY (article_id) REFERENCES article(id),
     UNIQUE(user_id, article_id) 
  );
+ 
 INSERT INTO userlike (user_id, article_id) VALUES (1, 1);
 INSERT INTO userlike (user_id, article_id) VALUES (2, 3);
 INSERT INTO userlike (user_id, article_id) VALUES (3, 2);
 INSERT INTO userlike (user_id, article_id) VALUES (1, 2);
 
   
-/* create table named category ---- gli300 */
-DROP TABLE if exists category;
-  
-create table category (
 
- id integer NOT NULL PRIMARY KEY,
 
- name varchar(50) NOT NULL,
-
- description varchar(1000) NOT NULL,
-
- FOREIGN KEY (user_id) REFERENCES user (id)
-);
-insert into category (id, name, description) values (1, 'fiction', 'a type of literature that describes imaginary people and events, not real ones');  
-  
-/*table article: More info in interface.md; by zipei liu*/
-create table artical (
-    id integer not null primary key,
-    title text not null,
-    content text not null,
-    imagelocation text like '/images/%',  
-    postdate date not null like'_____-__-__',
-    userid integer,
-    account text,
-    foreign key userid references user(id),
-    foreign key account references user(account)
-);
-insert into artical(id, title, content,imagelocation, postdate, userid, account, userid, account)values
-    (0, 'Love', '<strong>I love you!</strong>', , '2023-10-9', 3, 'Zipei Liu')
-    (1, 'Boy', '<em>I am a boy!</em>','/images/boy.jpeg' , '2023-10-16', 5, 'Guanzhuo Li')
-    (2, 'Game', 'I love play game', ,,'2023-10-8',1,'Clarke' ) 
 
 
 /* comments table created and 4 rows data filled ---- txu470 */
@@ -79,16 +81,16 @@ DROP TABLE if exists comments;
 CREATE TABLE comments (
     id integer not null PRIMARY KEY,
     user_id integer,
-    timeDate TIMESTAMP,
+    timeDate integer,
     content TEXT, 
     parentComment integer,
     article_id integer,
-    FOREIGN KEY (user_id) REFERENCES users(id), 
-    FOREIGN KEY (article_id) REFERENCES articles(id) 
+    FOREIGN KEY (user_id) REFERENCES user(id), 
+    FOREIGN KEY (article_id) REFERENCES article(id)
 );
-INSERT INTO comments (user_id, timeDate,content,parentComment, article_id)
+INSERT INTO comments (user_id,content,parentComment, article_id)
 VALUES
-    (1, '2023-10-01 14:30:00','This is the first comment.', NULL, 1),
-    (2, '2023-10-02 15:15:00','Here is the second comment.', NULL, 2),
-    (3, '2023-10-03 16:00:00','A reply to the first comment.', 1, 1), 
-    (4, '2023-10-04 16:45:00','A reply to the second comment.',3, 1); 
+    (1,'This is the first comment.', NULL, 1),
+    (2,'Here is the second comment.', NULL, 2),
+    (3,'A reply to the first comment.', 1, NULL), 
+    (4,'A reply to the second comment.',3, NULL); 
