a8a350ab825afc38c074aa3a9af6b5d03bbcfdc8
Arcshic <20130421042jyl@gmail.com>
2023-10-10 23:17:18 +1300

    finish the login,register and delete method in DAO and routers for user

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const VERSION = '1.0.0';
 async function startServer() {
     console.log('-'.repeat(30));
     console.log(`Starting Server v${VERSION}`);
-    await initializeDatabase();
     await startExpress();
 }
 
--- src/routes/application-routes.js
+++ src/routes/application-routes.js
@@ -1,13 +1,68 @@
 const express = require('express');
 const router = express.Router();
 
-const testDao = require('../models/test-dao.js');
+const blogDao = require('../models/blog-dao.js');
 
-router.get('/', async function (req, res) {
-    res.locals.title = 'My route title!';
-    res.locals.allTestData = await testDao.retrieveAllTestData();
+const userid = 1;
 
-    res.render('home');
+// user router created ----- yji413
+router.post('/userLogin', async function (req, res) {
+    let { account, password } = req.body;
+    try {
+        let userDetails = await blogDao.searchUsersByAccount(account, password)
+        if (userDetails.length > 0) {
+            res.send({
+                code: 200,
+                msg: "Login successful",
+                data: userDetails[0]
+            })
+        } else {
+            res.send({
+                code: 500,
+                msg: "Login failed"
+            })
+
+        }
+    } catch (error) {
+        res.send({
+            code: 500,
+            msg: "Login failed"
+        })
+    }
 });
 
+router.post('/userRegister', async function (req, res) {
+    let { account, password, birthday, description } = req.body;
+    try {
+        await blogDao.registerUser(account, password, birthday, description)
+        res.send({
+            code: 200,
+            msg: "Register successful",
+        })
+    } catch (error) {
+        res.send({
+            code: 500,
+            msg: "Register failed"
+        })
+    }
+});
+
+router.delete('/userDelete',async function(req,res){
+    let id = req.query.id;
+    try {
+        await blogDao.deleteUser(id)
+        res.send({
+            code: 200,
+            msg: "Delete successful",
+        })
+    } catch (error) {
+        res.send({
+            code: 500,
+            msg: "Delete failed"
+        })
+    }
+})
+
 module.exports = router;
+
+
