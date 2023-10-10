981630347a3cd36a25a39a9e6ef8fad7bdea4814
Greg Xu <gregxu0105@gmail.com>
2023-10-10 21:21:12 +1300

    add delete function in DAO and router-txu470

--- src/models/blog-dao.js
+++ src/models/blog-dao.js
@@ -1,13 +1,40 @@
 const SQL = require('sql-template-strings');
-const {dbPromise} = require('../db/database.js');
+const { dbPromise } = require('../db/database.js');
 
-async function searchUsersByAccount(userName,password){
+async function searchUsersByAccount(userName, password) {
     const db = await dbPromise;
     const result = await db.all(SQL`select * from user where account = ${userName} AND password = ${password}`);
     return result;
 }
+// delete article by id  ------txu470
+async function deleteArticleById(id) {
+    const db = await dbPromise;
+    const result = await db.run(SQL`delete from article where id = ${id}`);
+    return result;
+}
+async function searchArticleById(id) {
+    const db = await dbPromise;
+    const result = await db.all(SQL`select * from article where id = ${id}`);
+    return result;
+}
+// delete comment by id  ------txu470
+async function deleteCommentById(id) {
+    const db = await dbPromise;
+    const result = await db.run(SQL`delete from comments where id = ${id}`);
+    return result;
+}
+async function searchCommentById(id) {
+    const db = await dbPromise;
+    const result = await db.all(SQL`select * from comments where id = ${id}`);
+    return result;
+}
+
 
 // Export functions.
 module.exports = {
-    searchUsersByAccount
+    searchUsersByAccount,
+    deleteArticleById,
+    searchArticleById,
+    deleteCommentById,
+    searchCommentById
 };
--- src/routes/application-routes.js
+++ src/routes/application-routes.js
@@ -5,7 +5,7 @@ const blogDao = require('../models/blog-dao.js');
 
 const userid = 1;
 router.post('/user', async function (req, res) {
-    let {account,password} = req.body;
+    let { account, password } = req.body;
     let userDetails = await blogDao.searchUsersByAccount(account, password)
     console.log(userDetails)
     if (userDetails.length > 0) {
@@ -22,6 +22,96 @@ router.post('/user', async function (req, res) {
 
     }
 });
+// delete article by id  ------txu470
+router.delete('/article/:id', async function (req, res) {
+    let id = req.params.id;
+    if (!await isOwner(userid, id)) {
+        // If user is not the owner of the article
+        return res.status(403).send({ code: 403, msg: 'Forbidden' });
+    }
+    let result = await blogDao.deleteArticleById(id);
+    if (result.changes > 0) {
+        res.send({
+            code: 200,
+            msg: "Delete successful"
+        })
+    } else {
+        res.send({
+            code: 500,
+            msg: "Delete failed"
+        })
+    }
+});
+// check if the user is the owner of the article with articleId----txu470
+async function isOwner(userid, articleId) {
+    let result = await blogDao.searchArticleById(articleId);
+    if (result.length > 0) {
+        if (result[0].userid == userid) {
+            return true;
+        }
+    }
+    return false;
+}
+//commenter delete comment by id  ------txu470
+async function isCommentOwner(userid, commentId) {
+    let result = await blogDao.searchCommentById(commentId);
+    if (result.length > 0) {
+        if (result[0].userid == userid) {
+            return true;
+        }
+    }
+    return false;
+}
+router.delete('/comment/:id', async function (req, res) {
+    let id = req.params.id;
+    if (!await isCommentOwner(userid, id)) {
+        // If user is not the owner of the comment
+        return res.status(403).send({ code: 403, msg: 'Forbidden' });
+    }
+    let result = await blogDao.deleteCommentById(id);
+    if (result.changes > 0) {
+        res.send({
+            code: 200,
+            msg: "Delete successful"
+        })
+    } else {
+        res.send({
+            code: 500,
+            msg: "Delete failed"
+        })
+    }
+});
+//ArticleOwner delete comment by id  ------txu470
+async function isArticleOwner(userid, articleId) {
+    let result = await blogDao.searchArticleById(articleId);
+    if (result.length > 0) {
+        if (result[0].userid == userid) {
+            return true;
+        }
+    }
+    return false;
+}
+router.delete('/article/:id/comment/:commentid', async function (req, res) {
+    let id = req.params.id;
+    let commentid = req.params.commentid;
+    if (!await isArticleOwner(userid, id)) {
+        // If user is not the owner of the article
+        return res.status(403).send({ code: 403, msg: 'Forbidden' });
+    }
+    let result = await blogDao.deleteCommentById(commentid);
+    if (result.changes > 0) {
+        res.send({
+            code: 200,
+            msg: "Delete successful"
+        })
+    } else {
+        res.send({
+            code: 500,
+            msg: "Delete failed"
+        })
+    }
+});
+
 
 
 module.exports = router;
