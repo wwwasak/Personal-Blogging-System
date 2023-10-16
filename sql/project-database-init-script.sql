274f9d1fae35d5405af6e66ecfb992f94d4d886e
Ricky97531 <zli178@aucklanduni.ac.nz>
2023-10-16 22:41:53 +1300

    api

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -8,14 +8,15 @@ create table user (
     password text,
 	birthday text,
 	description text,
-    token text
+    token text,
+    isAdmin BOOLEAN DEFAULT FALSE 
 );
 
-insert into user (id,account,password,birthday,description) values
-    (1,'Sean','123456','1983/02/24','This is the first user'),
-    (2,'Demo','456234','1990/05/27','This is the second user'),
-    (3,'Clarke','abcdefg','1995/04/23','This is the third user'),
-    (4,'Admin','1wde356fg','1993/02/23','This is the fourth user');
+insert into user (id,account,password,birthday,description,isAdmin) values
+    (1,'Sean','123456','1983/02/24','This is the first user',FALSE),
+    (2,'Demo','456234','1990/05/27','This is the second user',FALSE),
+    (3,'Clarke','abcdefg','1995/04/23','This is the third user',FALSE),
+    (4,'Admin','1wde356fg','1993/02/23','This is the fourth user',TRUE);
 
 
 
